3998eb03a09105c8227530510f98651415b2d236
João Oliveira <38820667+jotavare@users.noreply.github.com>
2023-03-16 00:36:09 +0000

    docs: add the README and reorganise the sources into minitalk/

--- minitalk/client_bonus.c
+++ minitalk/client_bonus.c
@@ -16,12 +16,12 @@
 // process using SIGUSR1 and SIGUSR2 signals
 
 // Iterate through each character of the message
-	
+
 // Iterate through each bit of the current character
-			
+
 // If the current bit is 1, send SIGUSR1 signal;
 // otherwise, send SIGUSR2 signal
-						
+
 // Sleep for 100 microseconds between each signal
 void	signal_action(int pid, char *str)
 {
@@ -32,10 +32,8 @@ void	signal_action(int pid, char *str)
 	while (*str)
 	{
 		c = *(str);
-
 		while (i < 8)
 		{
-
 			if (c << i & 0b10000000)
 			{
 				if (kill(pid, SIGUSR1) == -1)
@@ -57,16 +55,24 @@ void	signal_action(int pid, char *str)
 // Handles the SIGUSR1 signal by printing a message
 
 // Validate the PID argument
-	
+
 // Validate the number of arguments and the message argument
-		
+
 // Set up the signal handler for SIGUSR1 and SIGUSR2
-				
+
 // Set up the signal handler for SIGUSR1 and SIGUSR2
 void	handler(int sig)
 {
-	if (sig == SIGUSR1)
-		ft_printf("The signal was received successfully.\n");
+	static int	message_printed = 0;
+
+	if (!message_printed)
+	{
+		if (sig == SIGUSR1)
+		{
+			ft_printf("The signal was received successfully.\n");
+			message_printed = 1;
+		}
+	}
 }
 
 int	main(int argc, char **argv)
@@ -79,7 +85,6 @@ int	main(int argc, char **argv)
 		if (!ft_isdigit(argv[1][i++]))
 			if (argv[1][i++] == '\0')
 				ft_printf("Invalid PID.\n");
-
 	if (argc != 3 || argv[2] == 0)
 	{
 		ft_printf("Number of arguments invalid or the message is empty.\n");
@@ -91,7 +96,6 @@ int	main(int argc, char **argv)
 		sa_sig.sa_flags = SA_SIGINFO;
 		sigaction(SIGUSR1, &sa_sig, NULL);
 		sigaction(SIGUSR2, &sa_sig, NULL);
-
 		signal_action(ft_atoi(argv[1]), argv[2]);
 	}
 	return (EXIT_SUCCESS);
